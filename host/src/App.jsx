8872ecdb099307bd7d8125b14dfba927b7b7500b
Darsh <darshkpatel@gmail.com>
2023-04-22 14:38:43 +0530

    feat: basic poc

---
+++ host/src/App.jsx
@@ -0,0 +1,33 @@
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import React from "react";
+const Button = React.lazy(() => import("remoteApp/Button"));
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React + Module Federation</h1>
+      <div className="card">
+        <React.Suspense fallback="Loading Button">
+          <Button />
+        </React.Suspense>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+}
+
+export default App;
